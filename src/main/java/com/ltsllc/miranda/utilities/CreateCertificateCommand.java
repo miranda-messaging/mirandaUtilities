652047d94f4a538ef6a1ff4e3c0cd707e58e39d3
ltsllc <ltsllc.consulting@gmail.com>
2017-07-09 11:12:56 -0600

    migrating from clark.hobbie@gmail.com to ltsllc.consulting@gmail.com

--- src/main/java/com/ltsllc/miranda/utilities/CreateCertificateCommand.java
+++ src/main/java/com/ltsllc/miranda/utilities/CreateCertificateCommand.java
@@ -4,12 +4,10 @@ package com.ltsllc.miranda.utilities;
  * Created by clarkhobbie on 7/8/17.
  */
 public class CreateCertificateCommand extends UtilitiesCommand {
-    @Override
     public void go() {
 
     }
 
-    @Override
     public void check() {
 
     }
